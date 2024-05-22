b4ec99c54686ae1991e56e48483f6b1932b48f3d
wei0821 <harrylin149@gmail.com>
2024-05-22 15:57:07 +0800

    v3_林威呈

--- app/src/main/java/com/example/s1114643/MainActivity.kt
+++ app/src/main/java/com/example/s1114643/MainActivity.kt
@@ -1,68 +1,107 @@
 package com.example.s1114643
 
-import android.app.Activity
-import android.app.SearchManager
-import android.content.Intent
-import android.net.Uri
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.rounded.AccountBox
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.s1114643.ui.theme.S1114643Theme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-            Main()
-
+            S1114643Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    Main()
+                }
+            }
         }
     }
 }
 @Composable
-fun FirstScreen(NavController: NavController){
-    Column(modifier = Modifier
+fun FirstScreen(NavController: NavController) {
+    var title by remember { mutableStateOf("瑪利亞基金會服務總覽") }
+    Column(
+        modifier = Modifier
     ) {
-        Text(text = "簡介",color = Color.Blue)
+        Text(text = title, color = Color.Blue)
+        var appear by remember { mutableStateOf(true) }
+        Column {
+            AnimatedVisibility(
+                visible = appear,
+                enter = fadeIn(
+                    initialAlpha = 0.1f,
+                    animationSpec = tween(durationMillis = 1500)
+                ),
+                exit = fadeOut(
+                    animationSpec = tween(durationMillis = 1500)
+                )
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.service),
+                    contentDescription = "服務總覽"
+                )
+            }
+            AnimatedVisibility(
+                visible = !appear,
+                enter = fadeIn(
+                    initialAlpha = 0.1f,
+                    animationSpec = tween(durationMillis = 1500)
+                ),
+                exit = fadeOut(
+                    animationSpec = tween(durationMillis = 1500)
+                )
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.mee),
+                    contentDescription = "mee"
+                )
+            }
+            Button(
+                onClick = { appear = !appear }
+            ) {
+                if (appear) {
+                    Text(text = "作者:資管二A林威呈")
+                    title = "瑪利亞基金會服務總覽"
+                } else {
+                    Text(text = "服務總覽")
+                    title = "關於作者"
+                }
+            }
+        }
     }
 }
-
-
 @Composable
 fun SecondScreen(navController: NavController) {
     Column(modifier = Modifier
@@ -70,6 +109,7 @@ fun SecondScreen(navController: NavController) {
         Text(text = "主要機構",color = Color.Red)
     }
 }
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Main() {
@@ -78,7 +118,12 @@ fun Main() {
 
     Column {
         TopAppBar(
-            title = { Image(painter = painterResource(id = R.drawable.maria), contentDescription ="maria" ) },
+            title = {
+                Image(
+                    painter = painterResource(id = R.drawable.maria),
+                    contentDescription = "maria"
+                )
+            },
             actions = {
                 IconButton(
                     onClick = { showMenu = true }
@@ -91,11 +136,11 @@ fun Main() {
                 ) {
                     DropdownMenuItem(
                         text = { Text("簡介") },
-                        onClick = { navController.navigate("JumpFirst")})
+                        onClick = { navController.navigate("JumpFirst") })
 
                     DropdownMenuItem(
                         text = { Text("主要機構") },
-                        onClick = { navController.navigate("JumpSecond")})
+                        onClick = { navController.navigate("JumpSecond") })
                 }
 
             }
